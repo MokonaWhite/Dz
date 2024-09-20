fcc016649c8c50dec2dca3f40f11f5ee17256a89
user01 <d12w5t36853e2@yandex.ru>
2024-09-21 02:41:42 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+    
+        Console.WriteLine("Введите количество строк:");
+        int n = Convert.ToInt32(Console.ReadLine());
+        string[] inputArray = new string[n];
+
+        Console.WriteLine("Введите строки по одной:");
+        for (int i = 0; i < n; i++)
+        {
+            inputArray[i] = Console.ReadLine();
+        }
+
+   
+        string[] filteredArray = FilterStrings(inputArray);
+
+     
+        Console.WriteLine("Отфильтрованный массив:");
+        Console.WriteLine(string.Join(", ", filteredArray));
+        Console.ReadKey();
+    }
+
+    static string[] FilterStrings(string[] inputArray)
+    {
+      
+        int count = 0;
+        foreach (var str in inputArray)
+        {
+            if (str.Length <= 3)
+            {
+                count++;
+            }
+        }
+
+    
+        string[] outputArray = new string[count];
+        int index = 0;
+
+        foreach (var str in inputArray)
+        {
+            if (str.Length <= 3)
+            {
+                outputArray[index++] = str;
+            }
+        }
+
+        return outputArray;
+       
+    }
+   
+}
